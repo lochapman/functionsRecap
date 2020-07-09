69ba62c1f70fd1e0a70c5b9722191a3f51f25dc9
Lukas Chapman <lukas.chapman@computingforall.org>
2020-07-09 14:23:45 -0700

    new and improved!

---
+++ myfunctions.js
@@ -0,0 +1,26 @@
+let pokeArray = [
+    {name: 'bulbasaur', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'},
+    {name: 'ivysaur', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'},
+    {name: 'charmander', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'},
+    {name: 'charizard', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'},
+    {name: 'squirtle', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'},
+  
+];
+let index = 0;
+let secIndex = 1;
+
+function showIt() {
+let button = document.getElementById ('javaFlow'); {
+    button.addEventListener ('click', function (){; 
+    console.log ('click');
+    targetElement.src = pokeArray[index].image;
+    index++;
+    if (index >= pokeArray.length) {
+    index = 0; }
+
+function secondIt() {
+    targetElement.src = pokeArray[secIndex].image;
+    secIndex++;
+    if (secIndex >= pokeArray.length) {
+    secIndex = 1; } 
+}
